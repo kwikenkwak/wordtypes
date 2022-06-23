40dd456328626bc5f8080de26e83781f60a4dbf2
Willem Vanderhaeghe <kwikandkwak@gmail.com>
2022-06-23 11:36:09 +0200

    Added STATIC_ROOT

--- dictyper/settings.py
+++ dictyper/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$dz67wjw!u_(^_c#8x10h&34)s55q_f+!sw2oye3qrlj^a664x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -116,6 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
